09a2b5cad318ba50d1ff2ada2dbdd9404d84bb32
mrelashvili <mrmrela5@gmail.com>
2022-10-19 02:11:34 +0400

    update-v1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
+import alanBtn from '@alan-ai/alan-sdk-web';
+import NewsCards from './components/NewsCards/NewsCards';
+
+import wordsToNumbers from 'words-to-numbers';
+
+import classes from './App.module.css';
+
+const alanKey = 'de31ae2ad56c3e95d60f5889526aecda2e956eca572e1d8b807a3e2338fdd0dc/stage';
 
 function App() {
+
+  const [newsArticles, setNewsArticles] = useState([]);
+  const [activeArticle, setActiveArticle] = useState(-1);
+
+  useEffect(() => {
+    alanBtn({ 
+      key: alanKey,
+      onCommand: ({ command, articles, number }) => {
+        if (command === 'newHeadlines') {
+          setNewsArticles(articles);
+          setActiveArticle(-1);
+        } else if(command === 'highlight') {
+          setActiveArticle((prevValue => prevValue + 1))
+        } else if (command === 'open') {
+          const parsedNumber = number.length > 2 ? wordsToNumbers((number), { fuzzy: true }) : number;
+          const article = articles[parsedNumber - 1];
+
+          if (parsedNumber > articles.length) {
+            alanBtn().playText('Please try that again...');
+          } else if (article) {
+            window.open(article.url, '_blank');
+            alanBtn().playText('Opening...');
+          } else {
+            alanBtn().playText('Please try that again...');
+          }
+
+          window.open(articles[number].url, '_blank')
+        }
+      }
+     })
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.mainContainer}>
+      <div className={classes.logoContainer}>
+        <img src="https://voicebot.ai/wp-content/uploads/2019/10/alan.jpg" 
+        className={classes.alanLogo} 
+        alt="logo" />
+      </div>
+      <NewsCards articles={newsArticles} activeArticle={activeArticle} />
     </div>
   );
 }
